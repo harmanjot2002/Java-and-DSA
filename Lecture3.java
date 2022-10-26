212f4c3a96dce76b25fbb6d1aa503bf8fac187ab
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-26 20:36:04 +0530

    Variables and input-output

---
+++ Lecture3.java
@@ -0,0 +1,9 @@
+public class Lecture3 {
+    public static void main(String[] args) {
+        int money=2000;
+        System.out.println(money);
+        money=5000;
+        System.out.println(money);
+        // System.out.println("Hello world");
+    }
+}
