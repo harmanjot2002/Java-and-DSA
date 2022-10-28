b08258e040dc456edf61cad463601c8257876f3b
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-28 14:32:04 +0530

    Variables and input-output

--- Lecture3.java
+++ Lecture3.java
@@ -1,9 +1,25 @@
-public class Lecture3 {
+public class lecture3{
     public static void main(String[] args) {
         int money=2000;
         System.out.println(money);
         money=5000;
         System.out.println(money);
         // System.out.println("Hello world");
+        Boolean flag=true;
+        System.out.println(flag);
+        byte range=105;
+        System.out.println(range);
+        short loss=-50;
+        System.out.println(loss);
+        int profit=5000;
+        System.out.println(profit);
+        long prof=455559990;
+        System.out.println(prof);
+        double height=12.5;
+        System.out.println(height);
+        float depth=-32.3f;
+        System.out.println(depth);
+        char temp='a';
+        System.out.println(temp);
     }
 }
