bbf904015952309b600fb09eb9e278ee46fe43d2
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-25 19:30:19 +0530

    Java Methods

--- Lecture12.java
+++ Lecture12.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
+class Algebra{
+    int add(int a,int b){
+        int ans=a+b;
+        return ans;
+    }
+}
+
 public class Lecture12{
     public static void main(String[] args) {
-        
+        Algebra obj=new Algebra();
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        System.out.println("Sum of input numbers is");
+        int ans=obj.add(x,y);
+        System.out.println(ans);
     }
 }
