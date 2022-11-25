32181690b56bee9dbaab564eeb5da1c3601773bf
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-25 20:44:13 +0530

    Java Methods and Scope of Variables

--- Lecture13.java
+++ Lecture13.java
@@ -1,5 +1,36 @@
 import java.util.Scanner;
 
+class Algebra{
+    //Member variables:Class Level Scope(need to be declared inside class,but outside method)
+    int a=5; 
+    int b=10;
+    int add(){
+        return a+b; //15
+    } 
+
+    int sub(){
+        //Method Level Scope(need to be declared inside method)
+        int p=100;
+        int q=200;
+        return p-q; //-100
+    }
+
+    void demo(){
+        //Block level scope(need to be declared inside {} curly brackets)
+        int a1=50;
+        System.out.println(a1); //50
+        System.out.println(b1); //error
+        {
+            int b1=10;
+            System.out.println(b1); //10
+        }
+        {
+            int b1=20;
+            System.out.println(b1); //20
+        }
+    }
+}
+
 public class Lecture13{
     public static void main(String[] args) {
         
