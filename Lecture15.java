3c0bdc091c461df1c03b708d97ac9d1e1700e690
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-26 15:59:33 +0530

    Taking Input,Array Reference,Cloning and Copy

--- Lecture15.java
+++ Lecture15.java
@@ -10,5 +10,8 @@ public class Lecture15{
         for(int i=0;i<arr.length;i++){
             arr[i]=sc.nextInt();
         }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
