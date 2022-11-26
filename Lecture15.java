e2212aa240c254b24d7aff4d0eba97d399200862
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-26 17:54:39 +0530

    Taking Input,Array Reference,Cloning and Copy

--- Lecture15.java
+++ Lecture15.java
@@ -9,6 +9,11 @@ public class Lecture15{
         System.out.println();
     }
 
+    static void changeArray(int[] arr3){
+        for(int i=0;i<arr3.length;i++){
+            arr3[i]=0;
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
@@ -21,16 +26,30 @@ public class Lecture15{
         }
         printArray(arr); //5 6 7 8 9
 
+
         //Copy elements of arr to arr2
         int[] arr2=arr;
         printArray(arr2); //5 6 7 8 9
+
 
         //Changing values of arr2
         arr2[0]=0;
         arr2[1]=3;
         printArray(arr); //0 3 7 8 9
         printArray(arr2); //0 3 7 8 9
+        //Even if we are changing elements of arr2 here,it also changes values of arr because here only copy of reference is made,not of whole memory.
+        //This is called "Shallow Copy".
 
-        //Even if we are changing elements of arr2 here,it also changes values of arr because here only copy f reference is made,not of whole memory.
+
+        //Checking pass by value in arrays
+        //In integers,there was only pass by value since nothing was made in heap,but in arrays copy of array will only be formed because of pass by value in Java,but it changes value in addresses in heap where arrays is stored,once that method is executed it deletes new array from stack as stack doesn't store anything in itself forever,it gets deleted once its executed.
+        int[] arr3=new int[3];
+        arr3[0]=5;
+        arr3[1]=6;
+        arr3[2]=7;
+        changeArray(arr3);
+        for(int i=0;i<arr3.length;i++){
+            System.out.println(arr3[i]);
+        }
     }
 }
