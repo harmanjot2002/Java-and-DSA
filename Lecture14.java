3977db9d5e1e2dc70a497ad50c36217bf8883afd
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-26 15:22:58 +0530

    Arrays Introduction

--- Lecture14.java
+++ Lecture14.java
@@ -12,7 +12,7 @@ class ArraysExample{
         System.out.println(ages[0]);
         System.out.println(ages[1]);
         System.out.println(ages[2]);
-        //Length of array
+        //Length operation in array
         System.out.println(ages.length);
         System.out.println(weights.length);
         System.out.println(names.length);
@@ -22,6 +22,30 @@ class ArraysExample{
         int[][] matrix={{4,5,6},{3,4,5},{7,8,9}};
         System.out.println(matrix[0][0]);
         System.out.println(matrix[1][2]);
+
+        //Traversing over arrays
+        //1.For loop
+        for(int i=0;i<3;i++){
+            System.out.println(ages[i]);
+        }
+        // 2.For-each loop
+        for(int age : ages){
+            System.out.println(age);
+        }
+        //While loop
+        int i=0;
+        while(i<3){
+            System.out.println(ages[i]);
+            ++i;
+        }
+
+        //Traversing over multi-dimensional arr.
+        for(int k=0;k<3;k++){
+            for(int j=0;j<3;j++){
+                System.out.println(matrix[k][j]);
+            }
+        }
+
     } 
 }
 
