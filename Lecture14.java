f9328301db5ab91da43a27557ce44c24ed802b6f
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-26 15:12:34 +0530

    Arrays Introduction

--- Lecture14.java
+++ Lecture14.java
@@ -4,7 +4,7 @@ class ArraysExample{
     void demoArrays(){
         int[] ages=new int[3];
         float[] weights=new float[3];
-        String[] names=new String[3];
+        String[] names={"Rahul","Raj","Mohan"};
         ages[0]=34;
         ages[1]=12;
         ages[2]=45;
@@ -12,7 +12,13 @@ class ArraysExample{
         System.out.println(ages[0]);
         System.out.println(ages[1]);
         System.out.println(ages[2]);
-    }
+
+        //Multi-Dimensional Arrays
+        int[][] arr1=new int[5][3];
+        int[][] matrix={{4,5,6},{3,4,5},{7,8,9}};
+        System.out.println(matrix[0][0]);
+        System.out.println(matrix[1][2]);
+    } 
 }
 
 public class Lecture14{
