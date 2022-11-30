d4c792c299e2af7c5bffccf0a66d076d185957cf
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-30 07:41:43 +0530

    Introduction to 2-D arrays

--- Lecture20.java
+++ Lecture20.java
@@ -10,6 +10,7 @@ public class Lecture20{
         }
     }
 
+    //1.Addition of 2 matrices
     static void add(int[][] a,int r1,int c1,int[][] b,int r2,int c2){
         if(r1!=r2 || c1!=c2){
             System.out.println("Wrong Input-Addition not possible");
@@ -22,6 +23,23 @@ public class Lecture20{
             }
         }
         printMatrix(sum);
+    }
+
+    //2.Multiplication of 2 matrices
+    static void multiply(int[][] a,int r1,int c1,int[][] b,int r2,int c2){
+        if(c1!=r2){
+            System.out.println("Multiplication not possible - wrong dimension ");
+            return;
+        }
+        int[][] mul=new int[r1][c2];
+        for(int i=0;i<r1;i++){
+            for(int j=0;j<c2;j++){
+                for(int k=0;k<c1;k++){
+                    mul[i][j]+=(a[i][k]*b[k][j]);
+                }
+            }
+        }
+        printMatrix(mul);
     }
 
     public static void main(String[] args) {
@@ -51,45 +69,33 @@ public class Lecture20{
             }
         }
         add(arr,row,col,arr2,row2,col2);    
-        
-        int m,n;
+       
 
-        //Multiplication of matrices:
-        System.out.println("enter the number of rows=");
-        m=sc.nextInt();
-        System.out.println("enter the number of columns=");
-        n=sc.nextInt();
-        int arr1[][]=new int[m][n];
-        int arr3[][]=new int[m][n];
-        int ans[][]=new int[m][n];
-        int i,j,k;
-        System.out.println("enter the first matrix element=\n");
-        for(i=0;i<m;i++){
-            for(j=0;j<n;j++){
-                arr1[i][j]=sc.nextInt();
+        System.out.println("Enter the no of rows : ");
+        int row3 = sc.nextInt();
+        System.out.println("Enter the number of columns : ");
+        int col3 = sc.nextInt();
+        int arr3[][] = new int[row3][col3];
+        int total3 = row3 * col3;
+        System.out.println("Please enter " + total3 + " elements now.");
+        for (int i = 0; i < row3; i++) {
+            for (int j = 0; j < col3; j++) {
+                arr3[i][j] = sc.nextInt();
             }
         }
-        System.out.println("enter the Second matrix element=\n");
-        for(i=0;i<m;i++){
-            for(j=0;j<n;j++){
-                arr3[i][j]=sc.nextInt();
+        System.out.println("Enter the no of rows4 : ");
+        int row4 = sc.nextInt();
+        System.out.println("Enter the number of columns2 : ");
+        int col4 = sc.nextInt();
+        int arr4[][] = new int[row4][col4];
+        int total4 = row4 * col4;
+        System.out.println("Please enter " + total4 + " elements now.");
+        for (int i4 = 0; i4 < row4; i4++) {
+            for (int j4 = 0; j4 < col4; j4++) {
+                arr4[i4][j4] = sc.nextInt();
             }
         }
-        System.out.println("multiply of the matrix=\n");
-        for(i=0;i<m;i++){
-            for(j=0;j<n;j++){
-                ans[i][j]=0;
-                for(k=0;k<n;k++){
-                    ans[i][j]+=arr1[i][k]*arr3[k][j];
-                }
-            }
-        }
-        for(i=0;i<m;i++){
-            for(j=0;j<n;j++){
-                System.out.print(ans[i][j] + " ");
-            }
-            System.out.println("");
-        }
+        multiply(arr3,row3,col3,arr4,row4,col4);
     }
 }
 
@@ -97,7 +103,7 @@ public class Lecture20{
 Enter the no of rows : 
 2
 Enter the number of columns : 
-2  
+2
 Please enter 4 elements now.
 2 3
 3 2
@@ -110,23 +116,23 @@ Please enter 4 elements now.
 3 2
 4 6 
 6 4
-enter the number of rows=
+Enter the no of rows :
 3
-enter the number of columns=
+Enter the number of columns : 
 3
-enter the first matrix element=
-
+Please enter 9 elements now.
 1 1 1
 2 2 2
 3 3 3
-enter the Second matrix element=
-
+Enter the no of rows4 : 
+3
+Enter the number of columns2 : 
+3
+Please enter 9 elements now.
 1 1 1
 2 2 2
 3 3 3
-multiply of the matrix=
-
-6 6 6
+6 6 6 
 12 12 12
 18 18 18
  */
