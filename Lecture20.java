2d46fa2f0a09f1e8a531b90be21bdcf9ff3ad754
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-30 06:44:53 +0530

    Introduction to 2-D arrays

--- Lecture20.java
+++ Lecture20.java
@@ -50,6 +50,83 @@ public class Lecture20{
                 arr2[i2][j2] = sc.nextInt();
             }
         }
-        add(arr,row,col,arr2,row2,col2);       
+        add(arr,row,col,arr2,row2,col2);    
+        
+        int m,n;
+
+        //Multiplication of matrices:
+        System.out.println("enter the number of rows=");
+        m=sc.nextInt();
+        System.out.println("enter the number of columns=");
+        n=sc.nextInt();
+        int arr1[][]=new int[m][n];
+        int arr3[][]=new int[m][n];
+        int ans[][]=new int[m][n];
+        int i,j,k;
+        System.out.println("enter the first matrix element=\n");
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                arr1[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("enter the Second matrix element=\n");
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                arr3[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("multiply of the matrix=\n");
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                ans[i][j]=0;
+                for(k=0;k<n;k++){
+                    ans[i][j]+=arr1[i][k]*arr3[k][j];
+                }
+            }
+        }
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println("");
+        }
     }
 }
+
+/*
+Enter the no of rows : 
+2
+Enter the number of columns : 
+2  
+Please enter 4 elements now.
+2 3
+3 2
+Enter the no of rows2 : 
+2
+Enter the number of columns2 : 
+2
+Please enter 4 elements now.
+2 3
+3 2
+4 6 
+6 4
+enter the number of rows=
+3
+enter the number of columns=
+3
+enter the first matrix element=
+
+1 1 1
+2 2 2
+3 3 3
+enter the Second matrix element=
+
+1 1 1
+2 2 2
+3 3 3
+multiply of the matrix=
+
+6 6 6
+12 12 12
+18 18 18
+ */
