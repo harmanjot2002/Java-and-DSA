40467faea53f0ba6e27285266fe6c47a3dc8c487
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-04-08 11:48:57 +0530

    Datatypes

---
+++ AB/Datatypes/datatype.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class datatype{
+    public static void main(String[] args) {
+        // Scanner sc=new Scanner(System.in);
+        System.out.println("Range and Size of Integers: "+Integer.MIN_VALUE+" "+Integer.MAX_VALUE+" "+Integer.BYTES);
+        System.out.println("Range and Size of Byte: "+Byte.MIN_VALUE+" "+Byte.MAX_VALUE+" "+Byte.BYTES);
+        System.out.println("Range and Size of Short: "+Short.MIN_VALUE+" "+Short.MAX_VALUE+" "+Short.BYTES);
+        System.out.println("Range and Size of Long: "+Long.MIN_VALUE+" "+Long.MAX_VALUE+" "+Long.BYTES);
+        System.out.println("Range and Size of Float: "+Float.MIN_VALUE+" "+Float.MAX_VALUE+" "+Float.BYTES);
+        System.out.println("Range and Size of Double: "+Double.MIN_VALUE+" "+Double.MAX_VALUE+" "+Double.BYTES);
+        System.out.println("Range and Size of Character: "+Character.MIN_VALUE+" "+Character.MAX_VALUE+" "+Character.BYTES);
+        // System.out.println("Range and Size of Bool: "+Bool.MIN_VALUE+" "+Bool.MAX_VALUE+" "+Bool.BYTES);
+    }
+}
