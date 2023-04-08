b6d8b1ae5f5cc750f80ee7e85df51fa0c6580235
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-04-08 13:10:31 +0530

    Variables

---
+++ AB/Datatypes/variable.java
@@ -0,0 +1,12 @@
+class variable{
+    public static void main(String[] args) {
+        byte b=13;
+        System.out.println(b);
+        b=4;
+        System.out.println(b);
+        double d=2.5;
+        System.out.println(d);
+        float f=2.5;
+        System.out.println(f);
+    }
+}
