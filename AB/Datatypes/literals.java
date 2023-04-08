8d841b79fd1e3f7d752cfb37ceb4ee1db2c69703
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-04-08 15:47:58 +0530

    Literals

---
+++ AB/Datatypes/literals.java
@@ -0,0 +1,28 @@
+public class literals {
+
+    public static void main(String[] args) {
+        byte b=2;
+        short s=2;
+        int i=2;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+
+        byte b1=10;//decimal
+        byte b2=0b1010;//binary
+        byte b3=012;//octal
+        byte b4=0XA;//hexadecimal
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+        
+        long l=9999999999L;
+        float f=12.56f;
+        double d=12.56d;
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+    }
+    
+}
