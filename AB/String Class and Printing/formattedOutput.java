5da3c5df078ed7212fda3335baf386fe20f8e48c
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-04-12 07:05:40 +0530

    Formatted Output

--- AB/String Class and Printing/formattedOutput.java
+++ AB/String Class and Printing/formattedOutput.java
@@ -7,6 +7,15 @@ public class formattedOutput{
         System.out.printf("%3$s %2$f %1$d",x,y,str);
         //System.out.printf("Hello %d %f %c %s\n",x,y,z,str);
         //%o,%x,%e for float value
+
+         /*int a=10;//a=-10
+        System.out.printf("%05d",a);*/
+        
+        /*float a=123.45f;//3.45f
+        System.out.printf("%6.2f",a);*/
+        
+        String str1="Java";
+        System.out.printf("%20s",str1);
     }
     
 }
