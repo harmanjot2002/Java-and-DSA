77748f11250ad113c0bc41ac2f170e4985c20e78
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-04-13 18:05:28 +0530

    Rotating array elements by one

---
+++ AB/Arrays/rotating.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class rotating{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int A[]={3,9,7,8,12,6,15,5,4,10};
+        int temp=A[0];
+        for(int i=1;i<A.length;i++){
+            A[i-1]=A[i];
+        }
+        A[A.length-1]=temp;
+        for(int i=0;i<A.length;i++)
+            System.out.print(A[i]+" ");
+    }
+}
