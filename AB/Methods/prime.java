7a908b51525500a1137363146a2c022b4c82f22a
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-04-16 17:42:39 +0530

    Check prime

--- AB/Methods/prime.java
+++ AB/Methods/prime.java
@@ -2,18 +2,24 @@ import java.util.*;
 
 class prime{
     static boolean isPrime(int x){
-        int cnt=0;
-        for(int i=1;i<=x;i++){
-            if(x%i==0){
-                cnt++;
-            }
+        // int cnt=0;
+        // for(int i=1;i<=x;i++){
+        //     if(x%i==0){
+        //         cnt++;
+        //     }
+        // }
+        // if(cnt==2){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
+
+        for(int i=2;i<x/2;i++){
+            if(x%i==0)
+                return false;
         }
-        if(cnt==2){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return true;   
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
