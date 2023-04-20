8877f10407054e1b559d771ab07202e2e15d3e55
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-04-21 05:12:57 +0530

    Array of Objects

--- AB/OOP/ArrayOfObjects.java
+++ AB/OOP/ArrayOfObjects.java
@@ -1,3 +1,37 @@
+class Subject{
+    private String subID;
+    private String name;
+    private int maxMarks;
+    private int marksObtains;
+    public Subject(String subID,String name,int maxMarks){
+        this.subID=subID;
+        this.name=name;
+        this.maxMarks=maxMarks;
+    }
+    public String getSubID(){return subID;}
+    public String getName(){return name;}
+    public int getMaxMarks(){return maxMarks;}
+    public int getMarksObtains(){return marksObtains;}
+    public void setMaxMarks(int mm){
+        maxMarks=mm;
+    }
+    public void setMarksObtain(int m){
+        marksObtains=m;
+    }
+    boolean isQualified(){
+        return marksObtains>=maxMarks/10*4;
+    }
+    public String toString(){
+        return"\n SubjectID: "+subID+"\n Name "+name+"\n MarksObtained "+maxMarks;
+    }
+}
 public class ArrayOfObjects{
-    
+    public static void main(String[] args) {
+        Subject s[]=new Subject[3];
+        s[0]=new Subject("CS120","DSA",90);
+        s[1]=new Subject("CS34","AA",43);
+        s[2]=new Subject("BE34","C",89);
+        for(Subject i:s)
+            System.out.println(i);
+    }
 }
