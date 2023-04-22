99477916734dc4e33042071a449c786288d1a685
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-04-22 15:55:43 +0530

    Final Members

--- AB/Static and Final/FinalMembers.java
+++ AB/Static and Final/FinalMembers.java
@@ -1,4 +1,18 @@
+class Test2{
+    public final void show(){
+        System.out.println();
+    }
+}
+class Test1 extends Test2{
+    //public void show(){}
+    //gives error as final methods cannot be overloaded.
+}
+
 public class FinalMembers{
+    /*final float PI;
+    public FinalDemo(){
+        PI=3.1425f;
+    }*/
     public static void main(String[] args) {
         final float PI;
         PI=3.1425f;
