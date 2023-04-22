62d3e805fb0be679c969873b1f5353909757b2c0
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-04-22 12:37:15 +0530

    Method Overriding

--- AB/Inheritance/MethodOverriding.java
+++ AB/Inheritance/MethodOverriding.java
@@ -4,8 +4,12 @@ class SuperClass{
     }
 }
 class SubClass extends SuperClass{
+    @Override
     public void display(){
         System.out.println("Hello Welcome");
+    }
+    public void display2(){
+        System.out.println("Method of only sub class");
     }
 }
 
@@ -13,12 +17,15 @@ public class MethodOverriding{
     public static void main(String[] args) {
         SuperClass sup=new SuperClass();
         sup.display();
+        // sup.display2();
 
         SubClass sub=new SubClass();
         sub.display();
+        // sub.display2();
 
         SuperClass su=new SubClass();
         su.display();
+        // su.display2();
 
     }
 }
