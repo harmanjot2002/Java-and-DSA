fc5ba6342bd43ab7904a17c9b470ac8c6722ea9a
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-04-22 11:15:39 +0530

    Using Super Keyword in inheritance

---
+++ AB/Inheritance/SuperConst.java
@@ -0,0 +1,33 @@
+class Rectangle{
+    int length;
+    int breadth;
+    Rectangle(){
+        length=breadth=1;
+    }
+    Rectangle(int l,int b){
+        length=l;
+        breadth=b;
+    }  
+}
+class Cuboid extends Rectangle{
+    int height;
+    Cuboid(){
+        height=1;
+    }
+    Cuboid(int h){
+        height=h;
+    }
+    Cuboid(int l,int b,int h){
+        super(l,b);
+        height=h;
+    }
+    int volume(){
+        return length*breadth*height;
+    }
+}
+public class SuperConst {
+    public static void main(String[] args) {
+        Cuboid c=new Cuboid(5,3,10);
+        System.out.println(c.volume());
+    }    
+}
