6c8fe1479ea1f0197ae2add7a6ee8889bac7faf0
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-04-24 14:12:37 +0530

    Week-1

--- Weekly PT/SmithNumber.java
+++ Weekly PT/SmithNumber.java
@@ -1,16 +1,47 @@
+/*/
+2
+4
+Smith Number
+8
+Not Smith Number
+ */
 import java.util.*;
 
 class SmithNumber{
+    static int SumOfDigits(int n){
+        int sum=0;
+        while(n>0){
+            sum+=n%10;
+            n/=10;
+        }
+        return sum;
+    }
+    static int SumOfPrime(int n){
+        int i=2;
+        int sum=0;
+        while(n>1){
+            if(n%i==0){
+                sum+=SumOfDigits(i);
+                n/=i;
+            }
+            else{
+                i++;
+            }
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int a=SumOfDigits(n);
-        int b=SumOfPrime(n);
-        if(a==b){
-            System.out.println("Smith Number");
-        }
-        else{
-            System.out.println("Not Smith Number");
+        int t=sc.nextInt();
+        while(t>0){
+            int n=sc.nextInt();
+            int a=SumOfDigits(n);
+            int b=SumOfPrime(n);
+            if(a==b)
+                System.out.println("Smith Number");
+            else
+                System.out.println("Not Smith Number");
+            t--;
         }
     }
 }
