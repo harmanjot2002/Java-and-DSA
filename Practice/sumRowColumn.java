9945d29044c9efb241b6021ae20cbc0d313c83bc
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-04-27 22:35:50 +0530

    Sum of Rows and Columns

--- Practice/sumRowColumn.java
+++ Practice/sumRowColumn.java
@@ -1,12 +1,14 @@
 import java.util.*;
 /*
- Enter elements of matrix: 
-1 2 3 4 5 6
+Enter elements of matrix: 
+1 2 6 5 3 4
 Column wise sum is: 
-9
-12
+10
+11
+11
 Row wise sum is:
 3
+11
 7
 11
  */
@@ -22,20 +24,28 @@ class sumRowColumn{
             }
         }
         System.out.println("Column wise sum is: ");
+        int max=0;
         for(i=0;i<2;i++){
             int scol=0;
             for(j=0;j<3;j++){
                 scol+=a[j][i];
             }
             System.out.println(scol);
+            if(max<scol)
+                max=scol;
         }
+        System.out.println(max);
         System.out.println("Row wise sum is: ");
+        int mx=0;
         for(i=0;i<3;i++){
             int srow=0;
             for(j=0;j<2;j++){
                 srow+=a[i][j];
             }
             System.out.println(srow);
+            if(mx<srow)
+                mx=srow;
         }
+        System.out.println(mx);
     }
 }
