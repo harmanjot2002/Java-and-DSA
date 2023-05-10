72e7bef19c170b09a5416318ac904942789bca5c
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-10 06:06:39 +0530

    Exception Handling

---
+++ AB/Exception Handling/TryCatch.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TryCatch {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 2 numbers:");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        try{
+            c=a/b;
+            System.out.println("Division is " +c);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Denominator should not be 0, try again");
+        }
+        System.out.println("Bye");        
+    } 
+}
