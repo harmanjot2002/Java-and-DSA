4581780075f44b6e3b19376afb5563bc6a827ffb
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-21 07:30:01 +0530

    Decimal to Binary

--- Practice/deciToBin.java
+++ Practice/deciToBin.java
@@ -2,13 +2,30 @@ import java.util.*;
 /*
  5
  101
+
+100000000
+101111101011110000100000000
  */
 class deciToBin{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        /*
         String ans=Integer.toBinaryString(n);
         int answer=Integer.parseInt(ans);
         System.out.println(answer);
+        */
+
+        String bin = "";
+        if(n == 0){
+            bin = "0";
+        }
+        else {
+            while(n > 0){
+                bin = (n % 2) + bin;
+                n = n / 2;
+            }
+        }
+        System.out.println(bin);
     }
 }
