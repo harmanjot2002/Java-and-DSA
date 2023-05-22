66698109ba63bb313f14dead3a41515dd738cdd1
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-22 18:33:45 +0530

    Generic Classes

--- AB/Generics/GenericClass.java
+++ AB/Generics/GenericClass.java
@@ -7,8 +7,11 @@ class GenericClass<T>{
         return obj;
     }
     public static void main(String[] args) {
-        GenericClass<Integer> d=new GenericClass<>();
-        d.setData(20);
+        // GenericClass<Integer> d=new GenericClass<>();
+        // d.setData(20);
+
+        GenericClass<String> d=new GenericClass<>();
+        d.setData("Hi 20");
         System.out.println(d.getData());
     }
 }
