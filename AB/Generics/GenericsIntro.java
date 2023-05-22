9dc72cd423af08c37071df0d02fdcbbf1d4e1e73
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-22 18:24:48 +0530

    Introduction to Generics

--- AB/Generics/GenericsIntro.java
+++ AB/Generics/GenericsIntro.java
@@ -2,7 +2,7 @@ class GenericsIntro<T>{
     T data[]=(T[]) new Object[3];
 
     public static void main(String[] args) {
-        GenericsIntro<String> gi=new GenericsIntro();        
+        GenericsIntro<String> gi=new GenericsIntro<>();        
         gi.data[0]="Hi";
         gi.data[1]="Hello";
         // gi.data[2]=10;      ->Compiler Error
