6a791ac721804ccd19ee9abf2b4332a5dd2daac2
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-22 06:43:01 +0530

    Largest Sum Subarray

--- Practice/LargestSumSubarray.java
+++ Practice/LargestSumSubarray.java
@@ -1,3 +1,23 @@
+/*
+ Given an array and you need to find out the contiguous subarray that has the largest sum
+and return the sum of the subarray
+Input Format
+A number representing n length of array. N lines representing the n elements of the array. The
+last line takes A number representing the target sum.
+Constraints
+The length of the array must be less than 10000
+Output Format
+Print the sum of the maximum subarray
+Sample Input
+9
+-2 1 3 4 -1 2 1 -5 4
+Sample Output
+6
+Explanation
+{-2,1,-3,4,-1,2,1,-5,4}
+The resultant will be 6.
+{4,-1,2,1}is the subarray that has the largest sum = 6.
+ */
 import java.util.*;
 public class LargestSumSubarray {
     public static void main (String args[]) {
@@ -16,7 +36,7 @@ public class LargestSumSubarray {
             sum += arr[i];
             ans = Math.max(ans, sum);
             if(sum<0)
-            sum=0;
+                sum=0;
         }
         return ans;
     }
