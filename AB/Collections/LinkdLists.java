126da8a75f409609f1770711bce92a4795ee9f4a
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-23 14:14:52 +0530

    Linked Lists

--- AB/Collections/LinkdLists.java
+++ AB/Collections/LinkdLists.java
@@ -8,24 +8,45 @@ class LinkdLists{
         for(int i=2;i<5;i++){
             ll.add(i);
         }
+        ll.addLast(40);
+        ll.addFirst(27);
         ll.add(5,70);
-        System.out.println(ll);  //[2, 3, 2, 3, 4, 70]
+        System.out.println(ll);  //[27, 2, 3, 2, 3, 70, 4, 40]
         
         ll.remove(3);
-        System.out.println(ll);  //[2, 3, 2, 4 ,70]
+        ll.removeFirst();
+        ll.removeLast();
+        System.out.println(ll);  //[2, 3, 3, 70 ,4]
         
         System.out.println(ll.size());  //5
         
         for (int i = 0; i < ll.size(); i++)
-            System.out.print(ll.get(i) + " ");  //2 3 2 4 70
+            System.out.print(ll.get(i) + " ");  //2 3 3 70 4
 
         System.out.println();
         Iterator itr=ll.iterator();
         while(itr.hasNext())
-            System.out.print(itr.next()); //232470
+            System.out.print(itr.next()); //233704
 
         System.out.println();
         ll.set(2,100);
-        System.out.println(ll);  //[2, 3, 100, 4, 70]
+        System.out.println(ll);  //[2, 3, 100, 70, 4]
+
+        for (Integer i : ll)
+            System.out.print(i + " "); //2 3 100 70 4
+
+        System.out.println();
+        System.out.println("Before sorting list:");
+        System.out.println(ll);  //[2, 3, 100, 70, 4]
+        Collections.sort(ll); 
+        System.out.println("after sorting list:");
+        System.out.println(ll); //[2, 3, 4, 70, 100]
+
+
+        System.out.println("LinkedList: "+ ll); //LinkedList: [2, 3, 4, 70, 100]
+        Object[] a = ll.toArray();
+        System.out.print("After converted LinkedList to Array: ");
+        for(Object o : a)
+        System.out.print(o+" ");  //After converted LinkedList to Array: 2 3 4 70 100
     }
 }
