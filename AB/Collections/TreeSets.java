d4ab9e47579c764265fdb74fd5d606e223b1ed6f
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-24 12:55:17 +0530

    Tree Sets

--- AB/Collections/TreeSets.java
+++ AB/Collections/TreeSets.java
@@ -6,32 +6,32 @@ class TreeSets{
         ts.add("Geek");
         ts.add("For");
         ts.add("Geeks");
-        System.out.println(ts);
+        System.out.println(ts); //[For, Geek, Geeks]
 
         String check="Geek";
-        System.out.println(ts.contains(check));
+        System.out.println(ts.contains(check)); //true
         
-        System.out.println(ts.first());
-        System.out.println(ts.last());
+        System.out.println(ts.first()); //For
+        System.out.println(ts.last()); //Geeks
 
         String val="Geek";
-        System.out.println(ts.higher(val));
-        System.out.println(ts.lower(val));
+        System.out.println(ts.higher(val)); //Geeks
+        System.out.println(ts.lower(val)); //For
 
         ts.add("A");
         ts.add("B");
         ts.add("C");
-        System.out.println(ts);
-        ts.remove("B");
-        System.out.println(ts);
+        System.out.println(ts); //[A, B, C, For, Geek, Geeks] 
+        ts.remove("B"); 
+        System.out.println(ts); //[A, C, For, Geek, Geeks]
 
         ts.pollFirst();
-        System.out.println(ts);
+        System.out.println(ts); //[C, For, Geek, Geeks]
         ts.pollLast();
-        System.out.println(ts);
+        System.out.println(ts); //[C, For, Geek]
 
         for(String str:ts){
-            System.out.print(str+" ");
+            System.out.print(str+" "); //C For Geek
         }
         System.out.println();
 
