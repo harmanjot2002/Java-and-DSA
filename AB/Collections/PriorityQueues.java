9cc60aafb659920c468a914b81e437974067ed12
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-25 20:53:57 +0530

    Priority Queues

--- AB/Collections/PriorityQueues.java
+++ AB/Collections/PriorityQueues.java
@@ -48,10 +48,31 @@ class PriorityQueues{
         pq1.add("Black");
         pq1.add("White");
         pq1.add("Red");
-        System.out.println(Collections.frequency(pq1,"Red"));
-        System.out.println("Original Priority Queue: "+pq1);
+        System.out.println(Collections.frequency(pq1,"Red"));  //2
+        System.out.println("Original Priority Queue: "+pq1);   //Original Priority Queue: [Black, Red, Green, White, Red]
         String str1;
         str1 = pq1.toString();
-        System.out.println("String representation of the Priority Queue: "+str1);  
+        System.out.println("String representation of the Priority Queue: "+str1);   //String representation of the Priority Queue: [Black, Red, Green, White, Red]
+
+
+
+        //Write a Java program to change priorityQueue to maximum priority queue.
+        PriorityQueue<Integer> pq2 = new PriorityQueue<>();
+        pq2.add(10);
+        pq2.add(22);
+        pq2.add(36);
+        pq2.add(25);
+        pq2.add(16);
+        pq2.add(70);
+        pq2.add(82);
+        pq2.add(89);
+        pq2.add(14);
+        System.out.println("Original Priority Queue: "+pq2); //Original Priority Queue: [10, 14, 36, 16, 22, 70, 82, 89, 25]
+        System.out.print("Maximum Priority Queue: ");
+        Integer val = null;
+        while( (val = pq2.poll()) != null) {
+            System.out.print(val+"  "); //Maximum Priority Queue: 10  14  16  22  25  36  70  82  89
+        }
+        System.out.print("\n");
     }
 }
