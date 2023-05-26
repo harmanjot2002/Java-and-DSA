e1400c820241c457ee3076d7338b852964684be8
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-26 11:35:25 +0530

    Tree Maps

--- AB/Collections/TreeMaps.java
+++ AB/Collections/TreeMaps.java
@@ -6,6 +6,42 @@ import java.util.*;
 
 class TreeMaps{
     static void printFreq(int arr[]){
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+
+        treeMap.put("A", 1);
+        treeMap.put("C", 3);
+        treeMap.put("B", 2);
+
+        int valueA = treeMap.get("A");
+        System.out.println("Value of A: " + valueA); //Value of A: 1
+
+        System.out.println(treeMap); //{A=1, B=2, C=3}
+        treeMap.remove("B");
+        System.out.println(treeMap); //{A=1, C=3}
+
+        System.out.println(treeMap.containsKey("A")); //true
+        System.out.println(treeMap.containsValue(3)); //true
+
+        System.out.println(treeMap.entrySet()); //[A=1, C=3]
+        
+        System.out.println(treeMap.firstEntry()); //A=1
+        
+        System.out.println(treeMap.firstKey()); //A
+
+        System.out.println(treeMap.headMap("B")); //{A=1}
+
+        System.out.println(treeMap.tailMap("B")); //{C=3}
+
+        System.out.println(treeMap.keySet()); //[A, C]
+
+        System.out.println(treeMap.lastKey()); //C        
+
+        System.out.println(treeMap.size()); //2
+
+        System.out.println(treeMap.subMap("A","C")); //{A=1}
+
+        System.out.println(treeMap.values()); //[1, 3]
+
         TreeMap<Integer,Integer> tm=new TreeMap<>();
         for(int i=0;i<arr.length;i++){
             Integer c=tm.get(arr[i]);
