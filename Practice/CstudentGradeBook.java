403954ee93afbd0d0e34f51c336e3af615c40b3e
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-26 18:18:47 +0530

    Student Grade Book

---
+++ Practice/CstudentGradeBook.java
@@ -0,0 +1,26 @@
+/*
+Write a Java program to create a grade book for students. The program should allow adding student names and their corresponding grades. Implement this using a collection (Map) where the student names are keys and the grades are values. The program should also provide functionality to retrieve the grade of a specific student and calculate the average grade of all students.
+ */
+import java.util.*;
+
+class CstudentGradeBook{
+    public static void main(String[] args) {
+        TreeMap<String,Integer> tm=new TreeMap<>();
+        tm.put("Harman",40);
+        tm.put("Navreet",90);
+        tm.put("Gurvansh",70);
+        System.out.println(tm);
+
+        if(tm.containsKey("Navreet")){
+            System.out.println(tm.get("Navreet"));
+        }
+
+       System.out.println(tm.values());
+       int sum=0;
+       for(Integer i:tm.values()){
+            sum+=i;
+       }
+       System.out.println(sum);
+       System.out.println(sum/3);
+    }
+}
